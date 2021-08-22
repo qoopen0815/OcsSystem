89fdcba933eb0723ca72ce59eb16ab9ad6237ad6
TomohiroKuwano <ku_engineer_wa@yahoo.com>
2021-08-22 21:30:22 +0900

    Add namespace

--- Scripts/Managements/GameManage.cs
+++ Scripts/Managements/GameManage.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManage : MonoBehaviour
+namespace Ocs.GameSystem
 {
-    private Ocs.Input.InputActions _input;
-
-    private void Awake() => this._input = new Ocs.Input.InputActions();
-    private void OnEnable() => this._input.Enable();
-    private void OnDisable() => this._input.Disable();
-    private void OnDestroy() => this._input.Dispose();
-    private void Start() => this._input.GameManager.QuitGame.started += context => QuitGame();
-
-    private void QuitGame()
+    public class GameManage : MonoBehaviour
     {
+        private Ocs.Input.InputActions _input;
+
+        private void Awake() => this._input = new Ocs.Input.InputActions();
+        private void OnEnable() => this._input.Enable();
+        private void OnDisable() => this._input.Disable();
+        private void OnDestroy() => this._input.Dispose();
+        private void Start() => this._input.GameManager.QuitGame.started += context => QuitGame();
+
+        private void QuitGame()
+        {
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+            UnityEditor.EditorApplication.isPlaying = false;
 #else
     Application.Quit();
-#endif   
+#endif
+        }
     }
 }
